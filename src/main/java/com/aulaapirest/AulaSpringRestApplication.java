5c1b4e20f402298b1e8fd8a57c79de22cc8d9809
Jean Fernandine <jfernandine@gmail.com>
2022-02-22 20:50:32 -0300

    Operacao de instanciacao

--- src/main/java/com/aulaapirest/AulaSpringRestApplication.java
+++ src/main/java/com/aulaapirest/AulaSpringRestApplication.java
@@ -1,13 +1,33 @@
 package com.aulaapirest;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class AulaSpringRestApplication {
+import com.aulaapirest.domain.Categoria;
+import com.aulaapirest.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class AulaSpringRestApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AulaSpringRestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
